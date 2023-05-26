bc240936ed05baf5be34eab36bb7eec788742394
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-05-26 10:40:22 +0600

    StateFulWidget #1

---
+++ lib/stateful_widget_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StateFulWidget extends StatefulWidget {
+  const StateFulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<StateFulWidget> createState() => _StateFulWidgetState();
+}
+
+class _StateFulWidgetState extends State<StateFulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
