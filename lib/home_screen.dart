bb72b83e065ca00398b4d15c606d02b5ad397059
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-05-26 10:23:28 +0600

    Home Screen #3

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
+  final int x = 0;
   @override
   Widget build(BuildContext context) {
     print('build');
@@ -11,8 +12,12 @@ class HomeScreen extends StatelessWidget {
         title: Text('Provider Basic'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-
+          Container(
+            child: Text(x.toString(), style: TextStyle(fontSize: 50,),),
+          )
         ],
       ),
     );
