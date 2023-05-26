bf2d980f5abd3596f124a3e0212a74a56149625d
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-05-26 10:30:14 +0600

    Home Screen #4

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+   HomeScreen({Key? key}) : super(key: key);
 
-  final int x = 0;
+   int x = 10;
   @override
   Widget build(BuildContext context) {
     print('build');
@@ -16,9 +16,16 @@ class HomeScreen extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Container(
-            child: Text(x.toString(), style: TextStyle(fontSize: 50,),),
+            child: Center(child: Text(x.toString(), style: TextStyle(fontSize: 50,),)),
           )
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          x++;
+          print(x);
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
