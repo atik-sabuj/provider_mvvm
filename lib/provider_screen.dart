9f82da51dda1ba334898a181eb03bd7052e22806
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-05-27 10:42:39 +0600

    Provider Screen #4

--- lib/provider_screen.dart
+++ lib/provider_screen.dart
@@ -18,7 +18,7 @@ class _ProviderScreenState extends State<ProviderScreen> {
     // TODO: implement initState
     super.initState();
 
-    Timer.periodic(Duration(microseconds: 1), (timer) {
+    Timer.periodic(Duration(seconds: 1), (timer) {
       count++;
       print(count);
       setState(() {
@@ -38,6 +38,11 @@ class _ProviderScreenState extends State<ProviderScreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          Center(
+            child: Text(DateTime.now().hour.toString()+":"+DateTime.now().minute.toString()+":"+DateTime.now().second.toString(),
+              style: TextStyle(fontSize: 50),
+              ),
+            ),
           Center(
             child: Text(count.toString(),
               style: TextStyle(fontSize: 50),
