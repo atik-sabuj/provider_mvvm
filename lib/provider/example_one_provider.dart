7a3e31910bcfca9ebd86f7f6d02edae41e5ad358
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-05-28 10:09:32 +0600

    Example One Screen #2

--- lib/provider/example_one_provider.dart
+++ lib/provider/example_one_provider.dart
@@ -10,6 +10,16 @@ class ExampleOneScreen extends StatefulWidget {
 class _ExampleOneScreenState extends State<ExampleOneScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Example One Screen'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+
+        ],
+      ),
+    );
   }
 }
