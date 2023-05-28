3b11726aad5fd962c595e5630e95213806ac455e
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-05-28 10:59:28 +0600

    Example One Screen #6

--- lib/screen/example_one.dart
+++ lib/screen/example_one.dart
@@ -13,7 +13,6 @@ class _ExampleOneScreenState extends State<ExampleOneScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ExampleOneProvider>(context, listen: true);
 
     print('build');
     return Scaffold(
@@ -23,38 +22,42 @@ class _ExampleOneScreenState extends State<ExampleOneScreen> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Slider(
-              min: 0,
-              max: 1,
-              value: provider.value, onChanged: (val){
-                provider.setValue(val);
+          Consumer<ExampleOneProvider>(builder: (context, value, child){
+            return Slider(
+                min: 0,
+                max: 1,
+                value: value.value, onChanged: (val){
+              value.setValue(val);
+            });
           }),
-          Row(
-            children: [
-              Expanded(
-                child: Container(
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: Colors.green.withOpacity(provider.value)
-                  ),
-                  child: const Center(
-                    child: Text('Container 1'),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  height: 100,
-                  decoration: BoxDecoration(
-                      color: Colors.red.withOpacity(provider.value)
-                  ),
-                  child: const Center(
-                    child: Text('Container 2'),
+          Consumer<ExampleOneProvider>(builder: (context, value, child){
+            return Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 100,
+                    decoration: BoxDecoration(
+                        color: Colors.green.withOpacity(value.value)
+                    ),
+                    child: const Center(
+                      child: Text('Container 1'),
+                    ),
                   ),
                 ),
-              ),
-            ],
-          )
+                Expanded(
+                  child: Container(
+                    height: 100,
+                    decoration: BoxDecoration(
+                        color: Colors.red.withOpacity(value.value)
+                    ),
+                    child: const Center(
+                      child: Text('Container 2'),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          }),
         ],
       ),
     );
