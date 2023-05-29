de0bef5ea29b3c98ffde075e2d5d72baf335e745
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-05-29 18:59:01 +0600

    Favorite Screen #8

---
+++ lib/screen/favorite/my_favorite.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyFavoriteItemScreen extends StatefulWidget {
+  const MyFavoriteItemScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MyFavoriteItemScreen> createState() => _MyFavoriteItemScreenState();
+}
+
+class _MyFavoriteItemScreenState extends State<MyFavoriteItemScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
