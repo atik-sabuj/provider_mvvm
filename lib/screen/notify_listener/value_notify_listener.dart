2e1db3320a4c0fb60478c89c3240e15a9dc35c33
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-05-30 12:06:36 +0600

    Notify Listener Screen #4

--- lib/screen/notify_listener/value_notify_listener.dart
+++ lib/screen/notify_listener/value_notify_listener.dart
@@ -1,28 +1,29 @@
 import 'package:flutter/material.dart';
 
-class NotifyListenerScreen extends StatefulWidget {
+class NotifyListenerScreen extends StatelessWidget {
   NotifyListenerScreen({Key? key}) : super(key: key);
 
   ValueNotifier<int> _counter = ValueNotifier<int>(0);
 
   @override
-  State<NotifyListenerScreen> createState() => _NotifyListenerScreenState();
-}
-
-class _NotifyListenerScreenState extends State<NotifyListenerScreen> {
-  @override
   Widget build(BuildContext context) {
+    print('build');
     return Scaffold(
       appBar: AppBar(
         title: Text('Notify Listener Page'),
       ),
-      body: const Center(
-        child: Text('Sabuj', style: TextStyle(fontSize: 50),),
+      body: Center(
+        child: ValueListenableBuilder(
+            valueListenable: _counter,
+            builder: (context, value, child){
+              return Text(_counter.value.toString(), style: TextStyle(fontSize: 50),);
+            }),
       ),
 
       floatingActionButton: FloatingActionButton(
         onPressed: (){
-
+          _counter.value++ ;
+          print(_counter.value.toString());
         },
         child: Icon(Icons.add),
       ),
