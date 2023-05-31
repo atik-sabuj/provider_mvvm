69a006040fe36e6dce63d57e45ba0cf55b9e89b8
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-05-31 11:16:50 +0600

    Login Screen #3

--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -8,16 +8,29 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Login Screen'),
       ),
-      body: Column(
-        children: [
-          
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Email',
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
